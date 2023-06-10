c5160c775ec0c5b86a6bf03d064625809c88e673
david <dfdavidfernando@gmail.com>
2023-06-09 22:30:38 -0500

    glup actualizacion 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const { src, dest, watch } = require('gulp');
 const sass = require("gulp-sass")(require('sass'));
+const plumber = require("gulp-plumber");
 function css(cb){
-    src('./src/scss/app.scss')// identificar el archivo de sass
+    src('./src/scss/**/*.scss')// identificar el archivo de sass
+    .pipe(plumber())
     .pipe( sass () ) // compilar el archivo de sass
     .pipe( dest('build/css'))  // Almacenar el archivo en el disco
 
@@ -9,7 +11,8 @@ function css(cb){
 }
 
 function dev(cb){
-    watch('./src/scss/app.scss', css) // vigilar los cambios en el archivo de sass
+    //el **/* es para que escuche todos los archivos dentro de la carpeta y los actualize
+    watch('./src/scss/**/*.scss', css) // vigilar los cambios en el archivo de sass
     cb();// callback
 }
 exports.css = css;
