42a70e2bacfcac323ad2bb3c6a2e96d6b15da38c
david <dfdavidfernando@gmail.com>
2023-06-11 19:44:28 -0500

    actualizacion gulp 4.1

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const cache = require('gulp-cache') // cache se eencarga de guardar archivos de 
 const imagemin = require('gulp-imagemin')// imagemin hace la conversion con mas calidad de imagenes
 const webp = require('gulp-webp');// convierte las imagenes a formato webp
 //funcion de integracion de scss a css con gulp
+const avif = require('gulp-avif');// convierte las imagenes a formato avif
 function css(cb){
     src('./src/scss/**/*.scss')// identificar el archivo de sass
     .pipe(plumber()) // evitar que se detenga la compilacion por errores
@@ -25,12 +26,23 @@ function imagenes(cb){
     .pipe( dest('build/img'))
     cb(); // callback function que
 }
+//funcion que convierte imagenes a formato webp
 function versionwebp(cb){
     const opciones = {
         quality: 50
     };
     src('./src/img/**/*.{png,jpg}')//se encarga de buscar todas las imagines en {png,jpg}estos formatos
     .pipe( webp( opciones) ) // compilar el archivo de sass
+    .pipe( dest('build/img'))  // Almacenar el archivo en el disco
+    cb(); // callback function que
+}
+//funcion que convierte inamgenes a formato avif
+function versionAvif(cb){
+    const opciones = {
+        quality: 50
+    };
+    src('./src/img/**/*.{png,jpg}')//se encarga de buscar todas las imagines en {png,jpg}estos formatos
+    .pipe( avif( opciones) ) // compilar el archivo de sass
     .pipe( dest('build/img'))  // Almacenar el archivo en el disco
     cb(); // callback function que
 }
@@ -44,4 +56,5 @@ function dev(cb){
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionwebp = versionwebp;
-exports.dev = parallel(imagenes,versionwebp,dev);//ejecuta las funciones en paralelo con parallel
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionAvif,versionwebp,dev);//ejecuta las funciones en paralelo con parallel
